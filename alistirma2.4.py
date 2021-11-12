55d45b0860feeb3bebf495ecdfe4aa49ce08435b
Ecem Sıla AŞICI <91960394+ecemasici@users.noreply.github.com>
2021-11-12 23:59:37 +0300

    Update alistirma2.4.py

--- alistirma2.4.py
+++ alistirma2.4.py
@@ -1,3 +1,4 @@
+#fibonacci fonksiyonu
 def fib(N):
     if N==1:
         return 1
@@ -5,6 +6,7 @@ def fib(N):
         return 1
     else:
         return fib(N-1) + fib(N-2)
-
+    
+#ilk 30 fibonacci sayısını ekrana yazdırmak için
 for i in range(1,31):
     print(fib(i),end=" ")
